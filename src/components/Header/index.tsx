bc0b312d1178211a37aed6805153f6ce9553418b
felipe <felipesscreator@gmail.com>
2022-08-30 17:09:30 -0300

    feat: modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import { Container, Content } from './styles';
 
-export function Header() {
+import logoImg from '../../assets/logo.svg';
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="money management" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nota transação
         </button>
       </Content>
